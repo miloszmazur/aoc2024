60889b514bd56850a4b12347650bdb0a53460fa0
Miłosz Mazur <milosz.mazur@hotmail.com>
2024-12-06 21:57:41 +0100

    oops fix input strings in benchmarks

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -16,40 +16,40 @@ fn day_01_part_2() {
 
 #[divan::bench]
 fn day_02_part_1() {
-    day_02::part1(divan::black_box(include_str!("../src/day_01/input.txt")));
+    day_02::part1(divan::black_box(include_str!("../src/day_02/input.txt")));
 }
 
 #[divan::bench]
 fn day_02_part_2() {
-    day_02::part2(divan::black_box(include_str!("../src/day_01/input.txt")));
+    day_02::part2(divan::black_box(include_str!("../src/day_02/input.txt")));
 }
 
 #[divan::bench]
 fn day_03_part_1() {
-    day_03::part1(divan::black_box(include_str!("../src/day_01/input.txt")));
+    day_03::part1(divan::black_box(include_str!("../src/day_03/input.txt")));
 }
 
 #[divan::bench]
 fn day_03_part_2() {
-    day_03::part2(divan::black_box(include_str!("../src/day_01/input.txt")));
+    day_03::part2(divan::black_box(include_str!("../src/day_03/input.txt")));
 }
 
 #[divan::bench]
 fn day_04_part_1() {
-    day_04::part1(divan::black_box(include_str!("../src/day_01/input.txt")));
+    day_04::part1(divan::black_box(include_str!("../src/day_04/input.txt")));
 }
 
 #[divan::bench]
 fn day_04_part_2() {
-    day_04::part2(divan::black_box(include_str!("../src/day_01/input.txt")));
+    day_04::part2(divan::black_box(include_str!("../src/day_04/input.txt")));
 }
 
 #[divan::bench]
 fn day_05_part_1() {
-    day_05::part1(divan::black_box(include_str!("../src/day_05/input.txt")));
+    day_05::part1(divan::black_box(include_str!("../src/day_05/input.txt"))).unwrap();
 }
 
 #[divan::bench]
 fn day_05_part_2() {
-    day_05::part2(divan::black_box(include_str!("../src/day_05/input.txt")));
+    day_05::part2(divan::black_box(include_str!("../src/day_05/input.txt"))).unwrap();
 }
