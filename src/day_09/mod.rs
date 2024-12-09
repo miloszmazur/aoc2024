fc263e9725e18d3a4b5fab8c0464d7cc7c41cf00
Miłosz Mazur <milosz.mazur@hotmail.com>
2024-12-09 19:20:45 +0100

    day 09 scaffold

---
+++ src/day_09/mod.rs
@@ -0,0 +1,24 @@
+use anyhow::Result;
+
+fn parse(input: &str) -> Result<&str> {
+    todo!()
+}
+
+pub fn part1(input: &str) -> Result<usize> {
+    todo!()
+}
+
+pub fn part2(input: &str) -> Result<usize> {
+    todo!()
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_p1() {
+        let input = "2333133121414131402";
+        let result = part1(input);
+        assert_eq!(result.unwrap(), 0);
+    }
+}
---
+++ src/exec/day_09.rs
@@ -0,0 +1,7 @@
+use aoc2024::day_09;
+
+fn main() {
+    let input = include_str!("../day_07/input.txt");
+    println!("Part 1: {}", day_09::part1(input).unwrap());
+    println!("Part 2: {}", day_09::part2(input).unwrap());
+}
